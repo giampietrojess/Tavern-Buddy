60906899cbfad1505888e104ab56baa9285c4c15
Jess Giampietro <giampietrojess@gmail.com>
2018-07-16 15:24:09 -0400

    add logo to example.js

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+
 export default class Example extends Component {
     render() {
         return (
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
-                        <div className="card">
+                        <img src={require('./images/TB-Logo.png')} />
+                        {/* <div className="card">
                             <div className="card-header">Example A Component</div>
 
                             <div className="card-body">
-                                I'm not an example component woo!
+                                I'm an example component yes!
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
