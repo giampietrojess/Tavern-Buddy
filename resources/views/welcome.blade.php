8d4bd897d2cd39febe0467269c72e5c2841f54df
Jess Giampietro <giampietrojess@gmail.com>
2018-07-16 15:26:16 -0400

    change title on welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel React application</title>
+        <title>Tavern Buddy</title>
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <h2 style="text-align: center"> Laravel + React application </h2>
         <div id="root"></div>
         <div id="example"></div>
         <script src="{{mix('js/app.js')}}" ></script>
