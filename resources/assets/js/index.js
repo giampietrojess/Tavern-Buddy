710748ef630eec1b4faf58406c9251fae55a5f84
Jess Giampietro <giampietrojess@gmail.com>
2018-07-16 15:24:01 -0400

    add home link to index component

---
+++ resources/assets/js/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/Example.js';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
+export default class Index extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Route path="/" exact component={Example}/>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Index />, document.getElementById('example'));
+}
