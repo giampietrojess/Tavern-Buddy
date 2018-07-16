622a76202bb1f71e0c1dd83bcdf720e7760f0159
Jess Giampietro <giampietrojess@gmail.com>
2018-07-16 13:54:26 -0400

    testing example component

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -11,7 +11,7 @@ export default class Example extends Component {
                             <div className="card-header">Example A Component</div>
 
                             <div className="card-body">
-                                I'm an example component woo!
+                                I'm not an example component woo!
                             </div>
                         </div>
                     </div>
