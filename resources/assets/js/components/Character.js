7175686f8a31e3e185aec391ce09bd771801dd35
Hillary Lavin <hilavin@gmail.com>
2018-07-16 19:35:33 -0400

    Create Character component

---
+++ resources/assets/js/components/Character.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+
+export default class Character extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            characters: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('/api/character').then(response => {
+            this.setState({
+                characters: response.data
+            });
+        }).catch(errors => {
+            console.log(errors);
+        })
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    
+                    <div className="col md 12">
+                        
+                            <img className="img-fluid" src={require('./images/TB-Logo-Better.png')} />
+                        
+                            <div className="card">
+                                <div className="card-header">Character Sheet</div>
+
+                                <div className="card-body">
+                                    Character Data
+                                    {this.state.characters.map(character => <li>{character.name}</li><li>{character.race}</li><li>{character.class}</li>)}
+                                </div>
+                            </div>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Character />, document.getElementById('example'));
+}
