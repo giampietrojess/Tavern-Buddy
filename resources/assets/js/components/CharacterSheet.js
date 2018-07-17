a0975544a6bf27baedda266586cb9909b18f8574
Hillary Lavin <hilavin@gmail.com>
2018-07-17 14:54:15 -0400

    Create class component in CharacterSheet, still trying to hook into API (create new branch for this)

--- resources/assets/js/components/CharacterSheet.js
+++ resources/assets/js/components/CharacterSheet.js
@@ -6,9 +6,38 @@ import axios from 'axios';
 
 export default class CharacterSheet extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: {}
+        };
+    }
+
+    componentDidMount() {
+        axios.get("/api/character/" + this.props.match.params.id).then(response => {
+            this.setState({
+                post : response.data[0]
+            });
+        }).catch(error => console.log(error));
+    }
     render() {
+        console.log(this.props);
         return (
-            <h1> Hello World! </h1>
+            <h1>{this.state.post.name}</h1>
         );
+
+
+        // if(this.state.post) {
+        //     return (
+        //         <div>
+        //             <h1>Character Name: {this.state.post.name} </h1>
+        //             <h3>Race: {this.state.post.race} </h3>
+        //             <h3>Class: {this.state.post.class} </h3>
+        //         </div>
+        //     );
+        // } else {
+        //     return null;
+        // }
+
     }
 }
